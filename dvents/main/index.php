d16fc7c4a985e9dd49fdeb87936c41db088e4f3c
dollarstir <kpin463@gmail.com>
2022-05-14 13:04:55 +0000

    sdfsdfs

--- dvents/main/index.php
+++ dvents/main/index.php
@@ -1,163 +1,184 @@
+<?php
+
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-<meta name="description" content="Colorlib Templates">
-<meta name="author" content="Colorlib">
-<meta name="keywords" content="Colorlib Templates">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Colorlib Templates">
+  <meta name="author" content="Colorlib">
+  <meta name="keywords" content="Colorlib Templates">
 
-<title>Apply for job by Colorlib</title>
+  <title> Adding Event</title>
 
-<style id="" media="all">@font-face {
-  font-family: 'Open Sans';
-  font-style: italic;
-  font-weight: 300;
-  font-stretch: normal;
-  font-display: swap;
-  src: url(/fonts.gstatic.com/s/opensans/v29/memQYaGs126MiZpBA-UFUIcVXSCEkx2cmqvXlWq8tWZ0Pw86hd0Rk5hkWVAexg.woff) format('woff');
-}
-@font-face {
-  font-family: 'Open Sans';
-  font-style: italic;
-  font-weight: 400;
-  font-stretch: normal;
-  font-display: swap;
-  src: url(/fonts.gstatic.com/s/opensans/v29/memQYaGs126MiZpBA-UFUIcVXSCEkx2cmqvXlWq8tWZ0Pw86hd0Rk8ZkWVAexg.woff) format('woff');
-}
-@font-face {
-  font-family: 'Open Sans';
-  font-style: italic;
-  font-weight: 600;
-  font-stretch: normal;
-  font-display: swap;
-  src: url(/fonts.gstatic.com/s/opensans/v29/memQYaGs126MiZpBA-UFUIcVXSCEkx2cmqvXlWq8tWZ0Pw86hd0RkxhjWVAexg.woff) format('woff');
-}
-@font-face {
-  font-family: 'Open Sans';
-  font-style: italic;
-  font-weight: 700;
-  font-stretch: normal;
-  font-display: swap;
-  src: url(/fonts.gstatic.com/s/opensans/v29/memQYaGs126MiZpBA-UFUIcVXSCEkx2cmqvXlWq8tWZ0Pw86hd0RkyFjWVAexg.woff) format('woff');
-}
-@font-face {
-  font-family: 'Open Sans';
-  font-style: italic;
-  font-weight: 800;
-  font-stretch: normal;
-  font-display: swap;
-  src: url(/fonts.gstatic.com/s/opensans/v29/memQYaGs126MiZpBA-UFUIcVXSCEkx2cmqvXlWq8tWZ0Pw86hd0Rk0ZjWVAexg.woff) format('woff');
-}
-@font-face {
-  font-family: 'Open Sans';
-  font-style: normal;
-  font-weight: 300;
-  font-stretch: normal;
-  font-display: swap;
-  src: url(/fonts.gstatic.com/s/opensans/v29/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsiH0B4gaVQ.woff) format('woff');
-}
-@font-face {
-  font-family: 'Open Sans';
-  font-style: normal;
-  font-weight: 400;
-  font-stretch: normal;
-  font-display: swap;
-  src: url(/fonts.gstatic.com/s/opensans/v29/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsjZ0B4gaVQ.woff) format('woff');
-}
-@font-face {
-  font-family: 'Open Sans';
-  font-style: normal;
-  font-weight: 600;
-  font-stretch: normal;
-  font-display: swap;
-  src: url(/fonts.gstatic.com/s/opensans/v29/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsgH1x4gaVQ.woff) format('woff');
-}
-@font-face {
-  font-family: 'Open Sans';
-  font-style: normal;
-  font-weight: 700;
-  font-stretch: normal;
-  font-display: swap;
-  src: url(/fonts.gstatic.com/s/opensans/v29/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsg-1x4gaVQ.woff) format('woff');
-}
-@font-face {
-  font-family: 'Open Sans';
-  font-style: normal;
-  font-weight: 800;
-  font-stretch: normal;
-  font-display: swap;
-  src: url(/fonts.gstatic.com/s/opensans/v29/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgshZ1x4gaVQ.woff) format('woff');
-}
-</style>
+  <style id="" media="all">
+    @font-face {
+      font-family: 'Open Sans';
+      font-style: italic;
+      font-weight: 300;
+      font-stretch: normal;
+      font-display: swap;
+      src: url(/fonts.gstatic.com/s/opensans/v29/memQYaGs126MiZpBA-UFUIcVXSCEkx2cmqvXlWq8tWZ0Pw86hd0Rk5hkWVAexg.woff) format('woff');
+    }
 
-<link href="css/css-main.css" rel="stylesheet" media="all">
-<meta name="robots" content="noindex, follow">
+    @font-face {
+      font-family: 'Open Sans';
+      font-style: italic;
+      font-weight: 400;
+      font-stretch: normal;
+      font-display: swap;
+      src: url(/fonts.gstatic.com/s/opensans/v29/memQYaGs126MiZpBA-UFUIcVXSCEkx2cmqvXlWq8tWZ0Pw86hd0Rk8ZkWVAexg.woff) format('woff');
+    }
+
+    @font-face {
+      font-family: 'Open Sans';
+      font-style: italic;
+      font-weight: 600;
+      font-stretch: normal;
+      font-display: swap;
+      src: url(/fonts.gstatic.com/s/opensans/v29/memQYaGs126MiZpBA-UFUIcVXSCEkx2cmqvXlWq8tWZ0Pw86hd0RkxhjWVAexg.woff) format('woff');
+    }
+
+    @font-face {
+      font-family: 'Open Sans';
+      font-style: italic;
+      font-weight: 700;
+      font-stretch: normal;
+      font-display: swap;
+      src: url(/fonts.gstatic.com/s/opensans/v29/memQYaGs126MiZpBA-UFUIcVXSCEkx2cmqvXlWq8tWZ0Pw86hd0RkyFjWVAexg.woff) format('woff');
+    }
+
+    @font-face {
+      font-family: 'Open Sans';
+      font-style: italic;
+      font-weight: 800;
+      font-stretch: normal;
+      font-display: swap;
+      src: url(/fonts.gstatic.com/s/opensans/v29/memQYaGs126MiZpBA-UFUIcVXSCEkx2cmqvXlWq8tWZ0Pw86hd0Rk0ZjWVAexg.woff) format('woff');
+    }
+
+    @font-face {
+      font-family: 'Open Sans';
+      font-style: normal;
+      font-weight: 300;
+      font-stretch: normal;
+      font-display: swap;
+      src: url(/fonts.gstatic.com/s/opensans/v29/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsiH0B4gaVQ.woff) format('woff');
+    }
+
+    @font-face {
+      font-family: 'Open Sans';
+      font-style: normal;
+      font-weight: 400;
+      font-stretch: normal;
+      font-display: swap;
+      src: url(/fonts.gstatic.com/s/opensans/v29/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsjZ0B4gaVQ.woff) format('woff');
+    }
+
+    @font-face {
+      font-family: 'Open Sans';
+      font-style: normal;
+      font-weight: 600;
+      font-stretch: normal;
+      font-display: swap;
+      src: url(/fonts.gstatic.com/s/opensans/v29/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsgH1x4gaVQ.woff) format('woff');
+    }
+
+    @font-face {
+      font-family: 'Open Sans';
+      font-style: normal;
+      font-weight: 700;
+      font-stretch: normal;
+      font-display: swap;
+      src: url(/fonts.gstatic.com/s/opensans/v29/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgsg-1x4gaVQ.woff) format('woff');
+    }
+
+    @font-face {
+      font-family: 'Open Sans';
+      font-style: normal;
+      font-weight: 800;
+      font-stretch: normal;
+      font-display: swap;
+      src: url(/fonts.gstatic.com/s/opensans/v29/memSYaGs126MiZpBA-UvWbX2vVnXBbObj2OVZyOOSr4dVJWUgshZ1x4gaVQ.woff) format('woff');
+    }
+  </style>
+
+  <link href="dvents/main/css/css-main.css" rel="stylesheet" media="all">
+  <meta name="robots" content="noindex, follow">
 </head>
+
 <body>
-<div class="page-wrapper bg-dark p-t-100 p-b-50">
-<div class="wrapper wrapper--w900">
-<div class="card card-6">
-<div class="card-heading">
-<h2 class="title">Apply for job</h2>
-</div>
-<div class="card-body">
-<form method="POST">
-<div class="form-row">
-<div class="name">Full name</div>
-<div class="value">
-<input class="input--style-6" type="text" name="full_name">
-</div>
-</div>
-<div class="form-row">
-<div class="name">Email address</div>
-<div class="value">
-<div class="input-group">
-<input class="input--style-6" type="email" name="email" placeholder="example@email.com">
-</div>
-</div>
-</div>
-<div class="form-row">
-<div class="name">Message</div>
-<div class="value">
-<div class="input-group">
-<textarea class="textarea--style-6" name="message" placeholder="Message sent to the employer"></textarea>
-</div>
-</div>
-</div>
-<div class="form-row">
-<div class="name">Upload CV</div>
-<div class="value">
-<div class="input-group js-input-file">
-<input class="input-file" type="file" name="file_cv" id="file">
-<label class="label--file" for="file">Choose file</label>
-<span class="input-file__info">No file chosen</span>
-</div>
-<div class="label--desc">Upload your CV/Resume or any other relevant file. Max file size 50 MB</div>
-</div>
-</div>
-</form>
-</div>
-<div class="card-footer">
-<button class="btn btn--radius-2 btn--blue-2" type="submit">Send Application</button>
-</div>
-</div>
-</div>
-</div>
+  <div class="page-wrapper bg-dark p-t-100 p-b-50">
+    <div class="wrapper wrapper--w900">
+      <div class="card card-6">
+        <div class="card-heading">
+          <h2 class="title">Add Events</h2>
+        </div>
+        <div class="card-body">
+          <form method="POST">
+            <div class="form-row">
+              <div class="name">Event Title</div>
+              <div class="value">
+                <input class="input--style-6" type="text" name="title">
+              </div>
+            </div>
+            <div class="form-row">
+              <div class="name">Venue</div>
+              <div class="value">
+                <div class="input-group">
+                  <input class="input--style-6" type="text" name="venue" placeholder="Venue">
+                </div>
+              </div>
+            </div>
 
-<script src="js/jquery-jquery.min.js"></script>
+            <div class="form-row">
+              <div class="name">Date</div>
+              <div class="value">
+                <div class="input-group">
+                  <input class="input--style-6" type="date" name="dt" placeholder="">
+                </div>
+              </div>
+            </div>
 
-<script src="js/7715-js-global.js"></script>
 
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+            <div class="form-row">
+              <div class="name">Date</div>
+              <div class="value">
+                <div class="input-group">
+                  <input class="input--style-6" type="time" name="tp" placeholder="">
+                </div>
+              </div>
+            </div>
+            
+            <div class="form-row">
+              <div class="name">Upload Image</div>
+              <div class="value">
+                <div class="input-group js-input-file">
+                  <input class="input-file" type="file" name="pic" id="file">
+                  <label class="label--file" for="file">Choose file</label>
+                  <span class="input-file__info">No file chosen</span>
+                </div>
+                <div class="label--desc">Upload event image</div>
+              </div>
+            </div>
+          </form>
+        </div>
+        <div class="card-footer">
+          <button class="btn btn--radius-2 btn--blue-2" type="submit">Submit</button>
+        </div>
+      </div>
+    </div>
+  </div>
 
-  gtag('config', 'UA-23581568-13');
-</script>
-<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"70b3ac835f876805","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2021.12.0","si":100}' crossorigin="anonymous"></script>
+  <script src="dvents/main/js/jquery-jquery.min.js"></script>
+
+  <script src="dvents/main/js/7715-js-global.js"></script>
+
+  
+ 
 </body>
+
 </html>
