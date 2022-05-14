822a435881a08aeac8ed4340f77e4ea22bafa604
dollarstir <kpin463@gmail.com>
2022-05-14 11:22:46 +0000

    vdf

--- engine.php
+++ engine.php
@@ -12,7 +12,35 @@ $router = new Router([
     new Route(
         '/home',
         function ($context) {
-            return Viewer::view('dvents/index.html', $context);
+            return Viewer::view('dvents/index.php', $context);
+        }
+    ),
+
+    new Route(
+        '/services',
+        function ($context) {
+            return Viewer::view('dvents/services/index.php', $context);
+        }
+    ),
+
+    new Route(
+        '/events',
+        function ($context) {
+            return Viewer::view('dvents/events/index.html', $context);
+        }
+    ),
+
+    new Route(
+        '/aboutus',
+        function ($context) {
+            return Viewer::view('dvents/about/index.html', $context);
+        }
+    ),
+
+    new Route(
+        '/contact',
+        function ($context) {
+            return Viewer::view('dvents/contact/index.html', $context);
         }
     ),
 ]);
